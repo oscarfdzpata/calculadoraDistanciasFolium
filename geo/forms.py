6c1992770613dfb8aba64aee911928bbe59d0cf6
Oscar Fernandez Pata <oscarfpata@gmail.com>
2021-07-01 18:03:49 +0200

    Añado validaciones en el form

--- geo/forms.py
+++ geo/forms.py
@@ -1,7 +1,48 @@
 from django import forms
 from .models import Measurement
 
+#Linrerias para validar el dato de entrada, que existe esa ubicacion
+from geopy.geocoders import Nominatim
+from geopy.distance import geodesic
+from .utils import get_geo,get_center_coordinates, get_zoom, get_ip_address
+
+import folium
+
 class MeasurementModelForm(forms.ModelForm):
+
+
     class Meta:
         model= Measurement
         fields = ('location','destination',)
+       
+
+    def clean_destination(self):
+        geolocator= Nominatim(user_agent='measurements')
+        destination_= self.cleaned_data.get('destination')  
+        destination=geolocator.geocode(destination_)       
+        if destination is None:
+            raise forms.ValidationError("La ubicacion de destino debe ser real")
+        
+        return destination
+
+
+    def clean_location(self):
+        geolocator= Nominatim(user_agent='measurements')
+        location_= self.cleaned_data.get('location')
+        location=geolocator.geocode(location_)
+        if location is None:         
+            raise forms.ValidationError("El origen debe ser una ubicacion real")
+
+        return location
+
+    '''
+    def clean(self):
+        geolocator= Nominatim(user_agent='measurements')
+        destination_= self.cleaned_data.get('destination')
+        destination=geolocator.geocode(destination_) 
+        if destination is None:
+            raise forms.ValidationError("La ubicacion de destino debe ser real")
+
+        return self.cleaned_data
+    '''
+
--- geo/views.py
+++ geo/views.py
@@ -27,50 +27,32 @@ def calculate_distance_view(request):
     location_= None
     destination_=None
 
+    
+    ''' Controlo el location desde la ip de origen
     ip= '90.170.170.12'
+    #obtenemos la ip
+    ip_= get_ip_address(request)
+    print("\n La ip de origen es:" , ip_)
     country, city, lat, lon = get_geo(ip)
     print('location country', country)
     print('location city', city)
     print('location lat', lat)
-    print('location lon', lon)
-
-        
+    print('location lon', lon)        
     location= geolocator.geocode(city)
     print("###", location)
+    '''
 
-    #obtenemos la ip
-    ip_= get_ip_address(request)
-    print("\n La ip de origen es:" , ip_)
 
-    #Pongo las de Obispo Lepe a mano
+
+    #Pongo el valor de origen por defecto en Obispo Lepe ####################################################
     origen_='obispo lepe, logroño'
     origen=geolocator.geocode(origen_)
-    location= origen
-    print(origen)
-    print(origen.latitude)        
-    #destino cordenadas
+    location= origen       
+    #origen cordenadas
     o_lat=origen.latitude
     print(origen.longitude)
     o_lon=origen.longitude
     pointO=(o_lat, o_lon)
-
-
-
-
-
-
-
-    #localizacion coordenadas
-    l_lat= lat
-    l_lon= lon
-    pointA = (l_lat, l_lon)
-
-    #initial folium map
-    m= folium.Map(width=800, heigth=500, location=pointA)
-    #location marker
-    folium.Marker([l_lat, l_lon], tooltip='click aqui para ver mas', popup=city['city'],
-                icon=folium.Icon(color='purple')).add_to(m)
-
     #initial folium map con Obispo Lepe
     #m= folium.Map(width=800, heigth=500, location=pointO)
     m= folium.Map(width="100%", heigth=500, id="mapa", location=get_center_coordinates(o_lat, o_lon), zoom_start=100)
@@ -78,76 +60,15 @@ def calculate_distance_view(request):
     folium.Marker([o_lat, o_lon], tooltip='click aqui para ver mas', popup=origen,
                 icon=folium.Icon(color='purple')).add_to(m)
 
-    if form.is_valid():
-        instance=form.save(commit=False)
-        #instance.destination=form.cleaned_data.get('destination')
-        destination_=form.cleaned_data.get('destination')        
-        destination=geolocator.geocode(destination_)
+    
 
-        #Comprobamos el valor de destination
-        if destination is None:
-            print ("\n Valor de destination incorrecto")
- 
-        
-        try:
-
-            print(destination)
-            print(destination.latitude)        
-            #destino cordenadas
-            d_lat=destination.latitude
-            print(destination.longitude)
-            d_lon=destination.longitude
-
-            #origen cordenadas desde el form
-            location_=form.cleaned_data.get('location')        
-            location=geolocator.geocode(location_)
-            print(location)
-            print(location.latitude)        
-            #origen cordenadas
-            o_lat=location.latitude
-            print(location.longitude)
-            o_lon=location.longitude
-            pointO = (o_lat, o_lon)
-
-
-            pointB = (d_lat, d_lon)
-            #calculo de distancias
-            distance=round(geodesic(pointO,pointB).km,2)
-
-            #folium map modification
-            #m= folium.Map(width=800, heigth=500, location=pointO)
-            m= folium.Map(width="100%", heigth=500, location=get_center_coordinates(o_lat, o_lon, d_lat, d_lon), zoom_start=get_zoom(distance)) #zoom_start=15
-            #location marker
-            folium.Marker([o_lat, o_lon], tooltip='click aqui para ver mas', popup=location, #popup=origen,
-                    icon=folium.Icon(color='purple')).add_to(m)
-            #destination marker
-            folium.Marker([d_lat, d_lon], tooltip='click aqui para ver mas', popup=destination,
-                    icon=folium.Icon(color='red', icon='cloud')).add_to(m)
-
-
-            #Draw the line beetewn location and destination
-            line= folium.PolyLine(locations=[pointO,pointB], weight=2, color='blue')
-            m.add_child(line)
-
-            instance.destination=destination
-            instance.location=location
-            instance.distance= distance
-            instance.save()
-            obj=Measurement.objects.last()
-        except Exception as e:
-            print("\n Ocurrio algun error, puede ser el location o el destination: ", e)
-
-
-    obj_measurement= Measurement.objects.all().order_by('-id')[:5]
 
     if request.method=="GET":
-        print("\n entra por get")
         try:        
             last_measeure_save=Measurement.objects.all().last()
             destination_= last_measeure_save.destination
             destination=geolocator.geocode(destination_)
-            print("\n Destination_ en Get:", destination_)
-            print("\n Destination en Get:", destination)
+            #destination=destination_
 
             #destino cordenadas
             d_lat=destination.latitude
@@ -157,11 +78,10 @@ def calculate_distance_view(request):
             #origen cordenadas desde el form
             location_=last_measeure_save.location
             location=geolocator.geocode(location_)
-            print(location)
-            print(location.latitude)        
+            #location=location_
+        
             #origen cordenadas
             o_lat=location.latitude
-            print(location.longitude)
             o_lon=location.longitude
             pointO = (o_lat, o_lon)
 
@@ -187,6 +107,63 @@ def calculate_distance_view(request):
 
         except Exception as e:
             print("\n Ocurrio algun error, puede ser el location o el destination: ", e)
+
+
+
+    if request.method=="POST":
+        if form.is_valid():
+            instance=form.save(commit=False)
+            #instance.destination=form.cleaned_data.get('destination')
+            destination_=form.cleaned_data.get('destination')  
+            destination=destination_
+                
+            try:
+
+      
+                #destino cordenadas
+                d_lat=destination.latitude
+                d_lon=destination.longitude
+
+                #origen cordenadas desde el form
+                location_=form.cleaned_data.get('location')        
+                location=location_
+
+                #origen cordenadas
+                o_lat=location.latitude
+                o_lon=location.longitude
+                pointO = (o_lat, o_lon)
+
+
+                pointB = (d_lat, d_lon)
+                #calculo de distancias
+                distance=round(geodesic(pointO,pointB).km,2)
+
+                #folium map modification
+                #m= folium.Map(width=800, heigth=500, location=pointO)
+                m= folium.Map(width="100%", heigth=500, location=get_center_coordinates(o_lat, o_lon, d_lat, d_lon), zoom_start=get_zoom(distance)) #zoom_start=15
+                #location marker
+                folium.Marker([o_lat, o_lon], tooltip='click aqui para ver mas', popup=location, #popup=origen,
+                        icon=folium.Icon(color='purple')).add_to(m)
+                #destination marker
+                folium.Marker([d_lat, d_lon], tooltip='click aqui para ver mas', popup=destination,
+                        icon=folium.Icon(color='red', icon='cloud')).add_to(m)
+
+
+                #Draw the line beetewn location and destination
+                line= folium.PolyLine(locations=[pointO,pointB], weight=2, color='blue')
+                m.add_child(line)
+
+                instance.destination=destination
+                instance.location=location
+                instance.distance= distance
+                instance.save()
+                obj=Measurement.objects.last()
+            except Exception as e:
+                print("\n Ocurrio algun error, puede ser el location o el destination: ", e)
+        else:
+            print("\n FORM ERRORs ", form.errors )
+
+    obj_measurement= Measurement.objects.all().order_by('-id')[:5]
 
 
 
