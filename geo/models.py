15647674a31f7db3db34da0f86cc06cd22cb9c27
Oscar Fernandez Pata <oscarfpata@gmail.com>
2021-07-01 18:12:47 +0200

    Cambios simples sin importancia

--- geo/models.py
+++ geo/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Measurement(models.Model):
-    location=models.CharField('Origen', max_length=200, default='Obispe Lepe, Logroño')
+    location=models.CharField('Origen', max_length=200, default='Obispo Lepe, Logroño')
     destination= models.CharField('Destino',max_length=200)
     distance= models.DecimalField(max_digits=10, decimal_places=2)
     created= models.DateTimeField(auto_now_add=True)
